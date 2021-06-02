34edac57183753281e6792ae4608629c4c403151
Magoninho <magoninhogamer@gmail.com>
2021-06-02 19:44:53 -0300

    notes

--- raycast.js
+++ raycast.js
@@ -31,12 +31,12 @@ class Map {
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
   }
-  
-  // a very userful function for checking if there is a wall at a point  
+
+  // a very userful function for checking if there is a wall at a point
   hasWallAt(x, y) {
     return this.grid[Math.floor(y / TILE_SIZE)][Math.floor(x / TILE_SIZE)];
   }
-  
+
   render() {
     for (var i = 0; i < MAP_NUM_ROWS; i++) {
       for (var j = 0; j < MAP_NUM_COLS; j++) {
@@ -62,26 +62,26 @@ class Player {
     this.moveSpeed = 2.5;
     this.rotationSpeed = 2 * (Math.PI / 180);
   }
-  
+
   update() {
     this.rotationAngle += this.turnDirection * this.rotationSpeed;
-    
+
     // if (!grid.hasWallAt(this.x, this.y)) {
       // only do that if the player is not colliding
       let moveStep = this.walkDirection * this.moveSpeed;
       this.x += Math.cos(this.rotationAngle) * moveStep;
       this.y += Math.sin(this.rotationAngle) * moveStep;
     // }
-    
+
     // reseting angle
     if (this.rotationAngle < 0)
       this.rotationAngle += 2 * Math.PI;
     if (this.rotationAngle > 2 * Math.PI)
       this.rotationAngle -= 2 * Math.PI;
-    
+
   }
-  
-  
+
+
   render() {
     noStroke();
     fill("red");
@@ -113,41 +113,41 @@ class Ray {
     this.isRayFacingRight = this.rayAngle < 0.5 * Math.PI || this.rayAngle > 1.5 * Math.PI;
     this.isRayFacingLeft = !this.isRayFacingRight;
   }
-  
+
   cast(columnId) {
     var xintersect, yintersect; // variables for storing the first intersection (that one that has the player position)
     var xstep, ystep;           // variables for storing the xstep and the ystep after finding the xintersect and yintersect
-    
+
     ////////////////////////////////////////////////
     // HORIZONTAL INTERSECTION CHECKING
     ///////////////////////////////////////////////
     var foundHorizontalWall = false; // we have to check if we found a wall (horizontal)
     var horizontalWallHitX = 0;      // var to store the X position of the wall in horizontal which was hit
     var horizontalWallHitY = 0;      // var to store the Y position of the wall in horizontal which was hit
-    
+
     yintersect = Math.floor(player.y / TILE_SIZE) * TILE_SIZE; // the position of the first intersection
 
     // TODO: explain the math behind this shit
     if (this.rayAngle > 0 && this.rayAngle < Math.PI) // looking down
       yintersect += TILE_SIZE;
-  
+
     xintersect = player.x + (yintersect - player.y) / Math.tan(this.rayAngle); // the x position of the first intersection
-    
+
     ystep = TILE_SIZE; // the y step for the horizontal checking will be the same as the tile size (only if the player is looking down)
     if (!(this.rayAngle > 0 && this.rayAngle < Math.PI)) // looking up
       ystep *= -1;
-    
+
     xstep = ystep/Math.tan(this.rayAngle); // TODO: explain the math behind this thing
 
     // the next intersection starts at the first intersection (with the player)
     var nextHorizontalX = xintersect;
     var nextHorizontalY = yintersect;
-    
+
     // TODO: explain the math behind this
     if (!(this.rayAngle > 0 && this.rayAngle < Math.PI)) // looking up (see that is the same as the above)
       nextHorizontalY -= 0.01;
-    
-    
+
+
     // checking the horizontal lines //
 
     // while the it is inside the window
@@ -156,7 +156,7 @@ class Ray {
       if (grid.hasWallAt(nextHorizontalX, nextHorizontalY)) {
         foundHorizontalWall = true;
         // we need to store the position of the wall found
-        horizontalWallHitX = nextHorizontalX; 
+        horizontalWallHitX = nextHorizontalX;
         horizontalWallHitY = nextHorizontalY;
         break;
       } else {
@@ -165,51 +165,51 @@ class Ray {
         nextHorizontalY += ystep;
       }
     }
-    
+
     /////////////////////////////////////
     // VERTICAL INTERSECTION CHECKING
     ////////////////////////////////////
     var foundVerticalWall = false;
     var verticalWallHitX = 0;
     var verticalWallHitY = 0;
-    
+
     xintersect = Math.floor(player.x / TILE_SIZE) * TILE_SIZE;
     if (this.rayAngle < 0.5 * Math.PI || this.rayAngle > 1.5 * Math.PI) // facing right
       xintersect += TILE_SIZE;
-    
+
     yintersect = player.y + (xintersect - player.x) * Math.tan(this.rayAngle); // TODO: see why is player.x + ...
-    
+
     xstep = TILE_SIZE;
-    
+
     if (!(this.rayAngle < 0.5 * Math.PI || this.rayAngle > 1.5 * Math.PI)) // facing left
       xstep *= -1;
-    
+
     ystep = xstep * Math.tan(this.rayAngle);
-    
+
     var nextVerticalX = xintersect;
     var nextVerticalY = yintersect;
-    
-    
+
+
     if (this.isRayFacingLeft)
       nextVerticalX -= 0.01;
-    
-    
-    
+
+
+
     while (nextVerticalX >= 0 && nextVerticalX <= WINDOW_WIDTH && nextVerticalY >= 0 && nextVerticalY <= WINDOW_HEIGHT) {
 
       if (grid.hasWallAt(nextVerticalX, nextVerticalY)) {
         foundVerticalWall = true;
         verticalWallHitX = nextVerticalX;
         verticalWallHitY = nextVerticalY;
-        
+
         break;
       } else {
         nextVerticalX += xstep;
         nextVerticalY += ystep;
       }
     }
-    
-    
+
+
     /////////////////////////////////////////////////////////////////////////
     // Distance calculation
     /////////////////////////////////////////////////////////////////////////
@@ -218,7 +218,7 @@ class Ray {
     /////////////////////////////////////////////////////////////////////////
     var horizontalDistance;
     var verticalDistance;
-    
+
     if (foundHorizontalWall) {
       horizontalDistance = distanceBetween(player.x, player.y, horizontalWallHitX, horizontalWallHitY);
     } else {
@@ -229,7 +229,7 @@ class Ray {
     } else {
       verticalDistance = Number.MAX_VALUE;
     }
-    
+
     this.wallHitX = (horizontalDistance < verticalDistance) ? horizontalWallHitX : verticalWallHitX;
     this.wallHitY = (horizontalDistance < verticalDistance) ? horizontalWallHitY : verticalWallHitY;
 
@@ -238,15 +238,15 @@ class Ray {
 
     if (verticalDistance < horizontalDistance) {
       this.color = 160;
-    } 
+    }
     if (horizontalDistance < verticalDistance) {
       this.color = 255;
     }
 
 
-    
+
   }
-  
+
   render() {
     stroke("red");
     line(
@@ -271,7 +271,7 @@ function keyPressed() {
     player.turnDirection = 1
   } else if (keyCode == LEFT_ARROW) {
     player.turnDirection = -1
-  } 
+  }
 }
 
 function keyReleased() {
@@ -283,7 +283,7 @@ function keyReleased() {
     player.turnDirection = 0
   } else if (keyCode == LEFT_ARROW) {
     player.turnDirection = 0
-  } 
+  }
 }
 
 function normalizeAngle(angle) {
@@ -333,36 +333,36 @@ function setup() {
 
 function update() {
   player.update();
-  
+
 }
 
 
 
-function draw() { 
-  update(); 
+function draw() {
+  update();
   image(bg,WINDOW_WIDTH,0, width, height);
   grid.render();
-  castAllRays(); 
-  
+  castAllRays();
+
   for (ray of rays) {
-    ray.render(); 
-  } 
+    ray.render();
+  }
   player.render();
-  
+
 
   for (var i = 0; i < NUM_RAYS; i++) {
     // TODO: figure out this formula
     var lineHeight = 32*(WINDOW_HEIGHT) / rays[i].distance;
-    
+
     var drawStart = -lineHeight / 2 + (WINDOW_HEIGHT + 100) / 2;
-    if(drawStart < 0) 
+    if (drawStart < 0)
       drawStart = 0;
     var drawEnd   = lineHeight / 2 + WINDOW_HEIGHT / 2;
-    if(drawEnd >= WINDOW_HEIGHT) 
+    if (drawEnd >= WINDOW_HEIGHT)
       drawEnd = WINDOW_HEIGHT - 1;
 
 
-    
+
     // where 3d stuff is being rendered
     noStroke();
     stroke(rays[i].color);
@@ -372,3 +372,8 @@ function draw() {
     strokeWeight(2);
   }
 }
+
+
+
+// TODO:  consider looking at this page if adding sprites in the future:
+// https://github.com/ssloy/tinyraycaster/wiki/Part-3:-populating-the-world
