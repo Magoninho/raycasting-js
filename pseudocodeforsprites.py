46bfb00d08278261c0374cae4b621ee2260e3e3e
Magoninho <magoninhogamer@gmail.com>
2021-07-16 21:32:40 -0300

    explanation

--- pseudocodeforsprites.py
+++ pseudocodeforsprites.py
@@ -1,3 +1,6 @@
+# This is just me trying to figure out an way of rendering sprites
+# Perhaps I'm not going to make it because I'm just tired of this project
+
 class Sprite:
     def __init__(self):
         self.x = x
@@ -12,6 +15,3 @@ class Sprite:
 
         angle = atan2(vecX, vecY)
         angle_diference = player.angle - angle
-
-
-
